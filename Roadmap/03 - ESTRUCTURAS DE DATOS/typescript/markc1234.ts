20e9b37043fb1de3a5b5390c98c86df4c98bee40
Marcos <markos3107@gmail.com>
2024-10-30 11:16:11 -0300

    03 - typescript

--- Roadmap/03 - ESTRUCTURAS DE DATOS/typescript/markc1234.ts
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/typescript/markc1234.ts
@@ -24,26 +24,7 @@
         name: string;
         age: number;
     }
-    let person: Person = { name: "Fabricio", age: 20 };
-    
-    // clases
-    class Animal {
-        name: string;
-        constructor(name: string) {
-            this.name = name;
-        }
-        move(distance: number = 0) {
-            console.log(`${this.name} moved ${distance} meters.`);
-        }
-    }
-    let dog = new Animal("Dog");
-    dog.move(10);
-
-    // genericos
-    function identity<T>(arg: T): T {
-        return arg;
-    }
-    let output = identity<string>("myString");
+    let person: Person = { name: "Fabricio", age: 20 }
     
     // maps y sets
     let map = new Map<string, number>();
@@ -54,24 +35,12 @@
     set.add(1);
     set.add(2);
 
-    // union types
-    let union: string | number;
-    union = "hello";
-    union = 42;
-
-    // intersection types
-    interface Name {
-        name: string;
-    }
-    interface Age {
-        age: number;
-    }
-    type PersonType = Name & Age;
-    let personIntersection: PersonType = { name: "Alice", age: 25 };
-
     // Utiliza operaciones de inserción, borrado, actualización y ordenación.
     arr2.push("A", "E", "I", "O", "U")
     arr.pop()
+
+    map.delete("two")
+    map.get("one")
 
     
     // DIFICULTAD EXTRA (opcional):
@@ -81,4 +50,6 @@
     // El programa solicita en primer lugar cuál es la operación que se quiere realizar, y a continuación los datos necesarios para llevarla a cabo.
     // El programa no puede dejar introducir números de teléfono no númericos y con más de 11 dígitos. (o el número de dígitos que quieras)
     // También se debe proponer una operación de finalización del programa.
+
+    
 })()
